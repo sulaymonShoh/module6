ac06c50c086c7c61f8e24bd6502730abe98f18b9
Sulaymonqori <ms13061312@gmail.com>
2024-01-15 00:15:37 +0500

    lesson2

--- lesson2/netflix/settings.py
+++ lesson2/netflix/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'netflix.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- lesson2/todo/views.py
+++ lesson2/todo/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Todo
+
 
 # Create your views here.
+def home_page(request):
+    return render(request, 'todo/home.html', context={"tfodos": Todo.objects.all()})
